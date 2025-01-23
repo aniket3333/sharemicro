a78a1cba5a8cc45ccfbc95bc819181b95bb9d78f
aniket <aniket.hiware3@gmail.com>
2025-01-23 16:44:27 +0530

    sas

--- src/app/share-point.service.ts
+++ src/app/share-point.service.ts
@@ -7,14 +7,36 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { ISharePointService } from './Ishare-point.service';
 import { UserModel } from './model/user.model';
 import { UserSearch } from './model/user.search.model';
+import { HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    Authorization: 'my-auth-token'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharePointService  implements ISharePointService{
-
+ 
   constructor(private http:HttpClient) { }
 
+  private getAuthToken(): string | null {
+    return localStorage.getItem('accesstoken');  // Get the token from localStorage (or sessionStorage)
+  }
+  private createHttpOptions(): { headers: HttpHeaders } {
+    const token = this.getAuthToken();  // Retrieve the token dynamically
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': token ? `Bearer ${token}` : ''  // Only add Authorization if token exists
+    });
+    return { headers };
+  }
+
+  
+ 
   getAllSites():Observable<BaseResponseModel<DataTableModel<SitesModal>>>{
 
     // return this.http.get("https://rnapi.sdaemon.com/Api/api/v1/SharePoint/GetSites")
@@ -57,6 +79,8 @@ export class SharePointService  implements ISharePointService{
     return this.http.get<BaseResponseModel<DataTableModel<UserModel>>>("https://rnapi.sdaemon.com/Api/api/v1/User/GetUserList",{params}
     );
   }
+
+ 
   deleteByIdUser(id:number):Observable<BaseResponseModel<DataTableModel<any>>>{
     
     let params = new HttpParams().set("Id", id)
@@ -64,11 +88,19 @@ export class SharePointService  implements ISharePointService{
     return this.http.delete<BaseResponseModel<DataTableModel<UserModel>>>("https://rnapi.sdaemon.com/Api/api/v1/User/DeleteUser",{params}
     );
   }
-  addUser(model:UserModel):Observable<BaseResponseModel<DataTableModel<UserModel>>>{
+  // addUser(model:UserModel):Observable<BaseResponseModel<DataTableModel<UserModel>>>{
     
   
-    return this.http.post<BaseResponseModel<DataTableModel<UserModel>>>("https://rnapi.sdaemon.com/Api/api/v1/Microsoft365User/CreateUser",{model}
-    );
+  //   return this.http.post<BaseResponseModel<DataTableModel<UserModel>>>("https://rnapi.sdaemon.com/Api/api/v1/Microsoft365User/CreateUser",{model}
+  //   );
+  // }
+
+  addUser(model: UserModel): Observable<BaseResponseModel<DataTableModel<UserModel>>> {
+    const url = 'https://rnapi.sdaemon.com/Api/api/v1/Microsoft365User/CreateUser';
+
+    const options = this.createHttpOptions();  // Get the HTTP options with Authorization header
+    
+    return this.http.post<BaseResponseModel<DataTableModel<UserModel>>>(url, { model }, options);
   }
 
 }
