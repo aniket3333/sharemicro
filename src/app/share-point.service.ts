68fb5e611925983f116e7094e5a530abfdf8e699
aniket <aniket.hiware3@gmail.com>
2025-01-23 19:48:29 +0530

    asa

--- src/app/share-point.service.ts
+++ src/app/share-point.service.ts
@@ -79,7 +79,9 @@ export class SharePointService  implements ISharePointService{
   viewDrivesfile(fileUrl:string):Observable<BaseResponseModel<DataTableModel<SitesModal>>>{
     // return this.http.get("https://rnapi.sdaemon.com/Api/api/v1/SharePoint/GetSites")
     let params = new HttpParams().set("fileUrl", fileUrl)
-    return this.http.get<BaseResponseModel<DataTableModel<SitesModal>>>("https://rnapi.sdaemon.com/Api/api/v1/SharePoint/NewReadExcelFile",{params}
+    const url = "https://rnapi.sdaemon.com/Api/api/v1/SharePoint/NewReadExcelFile";
+    const options = this.createHttpOptions(); 
+    return this.http.get<BaseResponseModel<DataTableModel<SitesModal>>>(url,{params,...options}
     );
   }
   getAccessToken(AuthorizationCode:any):Observable<BaseResponseModel<DataTableModel<any>>>{
